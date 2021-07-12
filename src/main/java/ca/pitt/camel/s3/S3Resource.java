753c0cd18fe1a40e8590b037c35904b3cd4ed89e
Andrew Pitt <apitt@redhat.com>
2021-07-12 14:39:02 -0400

    Upload and download from S3.

--- src/main/java/ca/pitt/camel/s3/S3Resource.java
+++ src/main/java/ca/pitt/camel/s3/S3Resource.java
@@ -1,7 +1,5 @@
 package ca.pitt.camel.s3;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.aws2.s3.AWS2S3Constants;
 import org.apache.camel.model.rest.RestBindingMode;
@@ -12,7 +10,7 @@ public class S3Resource extends RouteBuilder {
     public void configure() throws Exception {
 
         restConfiguration()
-            .bindingMode(RestBindingMode.json);
+            .bindingMode(RestBindingMode.auto);
 
         rest("/")
             .post()
@@ -20,22 +18,17 @@ public class S3Resource extends RouteBuilder {
 
         from("direct:dosomething")
             .log("Body: ${body}")
-            .setHeader(AWS2S3Constants.KEY, constant("test3.txt"))
+            .setHeader(AWS2S3Constants.KEY, simple("${in.header.filename}"))
             .convertBodyTo(String.class)
-            //.setBody(constant("${body}"))
             .to("aws2-s3://{{aws.bucketName}}?amazonS3Client=#noobaaClient");
 
         rest("/")
             .get()
-            .to("direct:getobjects");
+            .to("direct:getobject");
 
-        from("direct:getobjects").process(new Processor() {
-                @Override
-                public void process(Exchange exchange) throws Exception {
-                    exchange.getIn().setHeader(AWS2S3Constants.KEY, "test3.txt");
-                }
-            })
-            .to("aws2-s3://s3-camel?amazonS3Client=#noobaaClient&deleteAfterRead=false&operation=getObject")
+        from("direct:getobject")
+            .setHeader(AWS2S3Constants.KEY, simple("${in.header.filename}"))
+            .toD("aws2-s3://s3-camel?amazonS3Client=#noobaaClient&deleteAfterRead=false&operation=getObject")
             .convertBodyTo(String.class)
             .to("file:///tmp/files/?fileName=${header.CamelAwsS3Key}");
 
