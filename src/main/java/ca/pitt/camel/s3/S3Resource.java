b38f930014a384ae64545afbf06eacca82da6201
Andrew Pitt <apitt@redhat.com>
2021-07-12 08:35:55 -0400

    Finally fixed download from S3.

--- src/main/java/ca/pitt/camel/s3/S3Resource.java
+++ src/main/java/ca/pitt/camel/s3/S3Resource.java
@@ -1,5 +1,7 @@
 package ca.pitt.camel.s3;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.aws2.s3.AWS2S3Constants;
 import org.apache.camel.model.rest.RestBindingMode;
@@ -18,18 +20,25 @@ public class S3Resource extends RouteBuilder {
 
         from("direct:dosomething")
             .log("Body: ${body}")
-            .setHeader(AWS2S3Constants.KEY, constant("abc.txt"))
-            .setBody(constant("Andrew Test create s3 resource."))
+            .setHeader(AWS2S3Constants.KEY, constant("test3.txt"))
+            .convertBodyTo(String.class)
+            //.setBody(constant("${body}"))
             .to("aws2-s3://{{aws.bucketName}}?amazonS3Client=#noobaaClient");
 
         rest("/")
             .get()
             .to("direct:getobjects");
 
-        from("direct:getobjects")
-            .to("aws2-s3://{{aws.bucketName}}?amazonS3Client=#noobaaClient&prefix=blah.txt")
-            .to("file:/tmp/files");
- 
+        from("direct:getobjects").process(new Processor() {
+                @Override
+                public void process(Exchange exchange) throws Exception {
+                    exchange.getIn().setHeader(AWS2S3Constants.KEY, "test3.txt");
+                }
+            })
+            .to("aws2-s3://s3-camel?amazonS3Client=#noobaaClient&deleteAfterRead=false&operation=getObject")
+            .convertBodyTo(String.class)
+            .to("file:///tmp/files/?fileName=${header.CamelAwsS3Key}");
+
     }
 
 }
